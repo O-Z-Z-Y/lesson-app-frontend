9ea4fdf7dfd1224a1010ee58cf3c24ed37805f05
O-Z-Z-Y <lhsddp@naver.com>
2023-05-08 23:40:27 +0900

    feat: add-vuex-persistedstate

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,18 @@ import { createStore } from 'vuex'
 import Nav from './Nav'
 import modal from './modal'
 import courses from './courses'
+import createPersistedState from 'vuex-persistedstate';
 
 const store = createStore({
   modules: {
     Nav,
     Modal: modal,
     Courses: courses
-  }
+  },
+  plugins: [createPersistedState({
+    // paths에 입력한 모듈만 적용
+    paths: ['Nav','Courses'],
+  })],
 })
 
 export default store
